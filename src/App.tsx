e94faf30c5cc5eac6108ee4d14eb5a4a1c87d1fb
Olivier Ramier <olivier.ramier@gmail.com>
2021-02-21 21:38:09 +0100

    remove sugi

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Dagre } from "./Dagre";
-
-import { ChakraProvider } from "@chakra-ui/react";
-import { Sugiyama } from "./Sugiyama";
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
         <Dagre />
-        <Sugiyama />
       </ChakraProvider>
     </div>
   );
